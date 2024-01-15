71170afda578ed91430971e21f66d0bbe1bf0e76
baharkose <baharkse17@gmail.com>
2024-01-15 20:44:56 +0300

    added getActions

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,63 +1,81 @@
-import * as React from "react"
-import Box from "@mui/material/Box"
-import { DataGrid } from "@mui/x-data-grid"
-import { useSelector } from "react-redux"
+import * as React from "react";
+import Box from "@mui/material/Box";
+import {
+  DataGrid,
+  GridActionsCell,
+  GridActionsCellItem,
+} from "@mui/x-data-grid";
+import { useSelector } from "react-redux";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import useStockCalls from "../service/useStockCalls";
+import DeleteForever from "@mui/icons-material/DeleteForever";
 
-const columns = [
-  {
-    field: "_id",
-    headerName: "#",
-    flex: 1.4,
-    headerAlign: "center",
-    sortable: false,
-  },
-  {
-    field: "categoryId",
-    headerName: "Category",
-    flex: 1,
-    headerAlign: "center",
-    valueGetter: (params) => {
-      console.log(params)
-      return params.row.categoryId.name
-    },
-  },
-  {
-    field: "brandId",
-    headerName: "Brand",
-    flex: 1.2,
-    headerAlign: "center",
-    valueGetter: (params) => params.row.brandId.name,
-  },
-  {
-    field: "name",
-    headerName: "Name",
-    flex: 1.5,
-    headerAlign: "center",
-  },
-  {
-    field: "quantity",
-    headerName: "Stock",
-    type: "number",
-    flex: 1.5,
-    headerAlign: "center",
-  },
-  {
-    field: "actions",
-    headerName: "Actions",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    flex: 1.5,
-    headerAlign: "center",
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-]
+// hookları kullanırken dikkatli olmak lazım hoohları render hücrelerinin içerisinde kullanmayın. Çünkü hooklar nested yapılar içerisinde kullanılamaz. Hook tanımlanacaksa üstte tanımlanmalıdır.
 
 export default function ProductTable() {
-  const { products } = useSelector((state) => state.stock)
+  const { deleteStock } = useStockCalls();
+  const { products } = useSelector((state) => state.stock);
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "#",
+      flex: 1.4,
+      headerAlign: "center",
+      sortable: false,
+    },
+    {
+      field: "categoryId",
+      headerName: "Category",
+      flex: 1,
+      headerAlign: "center",
+      valueGetter: (params) => {
+        console.log(params);
+        return params.row.categoryId.name;
+      },
+    },
+    {
+      field: "brandId",
+      headerName: "Brand",
+      flex: 1.2,
+      headerAlign: "center",
+      valueGetter: (props) => props.row?.brandId?.name,
+      // tabloda başka verilere erişmek gerekirse value getter şeklinde yazmamız yeterli.
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1.5,
+      headerAlign: "center",
+    },
+    {
+      field: "quantity",
+      headerName: "Stock",
+      type: "number",
+      flex: 1.5,
+      headerAlign: "center",
+    },
+
+    // bu fonksiyon bize ne döndürüyor. Bu fonksiyon içierisine birçok veriye erişebilir. Bir sürü parametreleri var. İçinde bulunduğumuz rowun her türlü verisine buradan erişebiliyoruz.
+    //   valueGetter: (props) =>
+    //     `${props.row.firstName || ""} ${props.row.lastName || ""}`,
+    // },
+    {
+      field: "actions",
+      type: "actions",
+      getActions: (props: GridRowParams) => [
+        //  burda bir array döndürüyor.
+        <GridActionsCellItem
+          icon={<DeleteForever />}
+          onClick={() => deleteStock("products", props.id)}
+          label="Delete"
+        />,
+      ],
+    },
+  ];
 
   function getRowId(row) {
-    return row._id
+    return row._id;
   }
 
   return (
@@ -65,6 +83,7 @@ export default function ProductTable() {
       <DataGrid
         autoHeight
         rows={products}
+        // rowlar apiden gelecek olan veriler bizim için
         columns={columns}
         initialState={{
           pagination: {
@@ -79,5 +98,5 @@ export default function ProductTable() {
         getRowId={getRowId}
       />
     </Box>
-  )
+  );
 }
