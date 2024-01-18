32626e5358b136de3c9fc2bcf6015c4eb099ba3e
baharkose <baharkse17@gmail.com>
2024-01-18 22:09:38 +0300

    added charts

--- src/components/KPI.jsx
+++ src/components/KPI.jsx
@@ -1,25 +1,74 @@
-import React from "react";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PaymentsIcon from "@mui/icons-material/Payments";
-import { deepPurple } from "@mui/material/colors";
-import { Paper } from "@mui/material";
-import { Stack } from "@mui/material";
+import { amber, deepPurple, pink } from "@mui/material/colors";
+import { Avatar, Paper, Stack, Typography } from "@mui/material";
+import { Box } from "@mui/material";
+
+const kpiData = [
+  {
+    id: 1,
+    title: "sales",
+    amount: "$16900",
+    icon: <MonetizationOnIcon sx={{ fontSize: "2rem" }} />,
+    bgColor: deepPurple[100],
+    color: deepPurple[700],
+  },
+  {
+    id: 2,
+    title: "sales",
+    amount: "$16900",
+    icon: <ShoppingCartIcon sx={{ fontSize: "2rem" }} />,
+    bgColor: pink[100],
+    color: pink[700],
+  },
+  {
+    id: 3,
+    title: "sales",
+    amount: "$16900",
+    icon: <PaymentsIcon sx={{ fontSize: "2rem" }} />,
+    bgColor: amber[100],
+    color: amber[700],
+  },
+];
 
 const KPI = () => {
-  const kpiData = [
-    {
-      id: 1,
-      title: "sales",
-      amount: "$16900",
-      icon: <MonetizationOnIcon />,
-      bgColor: deepPurple[100],
-      color: deepPurple[600],
-    },
-  ];
   return (
-    <Stack justifyContent="center">
-      <Paper elevation={2}>kpi</Paper>
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      flexWrap={"wrap"}
+      direction={"row"}
+      gap={2}
+    >
+      {kpiData.map((item) => (
+        <Paper
+          key={item.id}
+          elevation={10}
+          sx={{
+            display: "flex",
+            p: 2,
+            gap: 2,
+            width: "250px",
+            alignItems: "center",
+          }}
+        >
+          <Avatar
+            sx={{
+              bgcolor: item.bgColor,
+              color: item.color,
+              width: "70px",
+              height: "70px",
+            }}
+          >
+            {item.icon}
+          </Avatar>
+          <Box>
+            <Typography variant="button">{item.title}</Typography>
+            <Typography variant="h5">{item.amount}</Typography>
+          </Box>
+        </Paper>
+      ))}
     </Stack>
   );
 };
