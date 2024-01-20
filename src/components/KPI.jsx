07cc4e84b4f98585a11de23fd0f003b27e66c3b8
baharkose <baharkse17@gmail.com>
2024-01-20 13:51:41 +0300

    added pruchases

--- src/components/KPI.jsx
+++ src/components/KPI.jsx
@@ -4,35 +4,42 @@ import PaymentsIcon from "@mui/icons-material/Payments";
 import { amber, deepPurple, pink } from "@mui/material/colors";
 import { Avatar, Paper, Stack, Typography } from "@mui/material";
 import { Box } from "@mui/material";
-
-const kpiData = [
-  {
-    id: 1,
-    title: "sales",
-    amount: "$16900",
-    icon: <MonetizationOnIcon sx={{ fontSize: "2rem" }} />,
-    bgColor: deepPurple[100],
-    color: deepPurple[700],
-  },
-  {
-    id: 2,
-    title: "sales",
-    amount: "$16900",
-    icon: <ShoppingCartIcon sx={{ fontSize: "2rem" }} />,
-    bgColor: pink[100],
-    color: pink[700],
-  },
-  {
-    id: 3,
-    title: "sales",
-    amount: "$16900",
-    icon: <PaymentsIcon sx={{ fontSize: "2rem" }} />,
-    bgColor: amber[100],
-    color: amber[700],
-  },
-];
+import { useSelector } from "react-redux";
 
 const KPI = () => {
+  const { sales, purchases } = useSelector((state) => state.stock);
+
+  const totalSales = sales?.reduce((acc, val) => acc + val.amount, 0);
+
+  const totalPurchases = purchases?.reduce((acc, val) => acc + val.amount, 0);
+
+  const kpiData = [
+    {
+      id: 1,
+      title: "sales",
+      amount: `$${totalSales}`,
+      icon: <MonetizationOnIcon sx={{ fontSize: "2rem" }} />,
+      bgColor: deepPurple[100],
+      color: deepPurple[700],
+    },
+    {
+      id: 2,
+      title: "purchases",
+      amount: `$${totalPurchases}`,
+      icon: <ShoppingCartIcon sx={{ fontSize: "2rem" }} />,
+      bgColor: pink[100],
+      color: pink[700],
+    },
+    {
+      id: 3,
+      title: "profit",
+      amount: "$16900",
+      icon: <PaymentsIcon sx={{ fontSize: "2rem" }} />,
+      bgColor: amber[100],
+      color: amber[700],
+    },
+  ];
+
   return (
     <Stack
       justifyContent="center"
@@ -45,6 +52,7 @@ const KPI = () => {
         <Paper
           key={item.id}
           elevation={10}
+          // bize shadow verir
           sx={{
             display: "flex",
             p: 2,
